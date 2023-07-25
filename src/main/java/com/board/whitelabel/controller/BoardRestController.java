d514176f03feab8d72c6a690616230211d344671
yuseob <dladbtjq@naver.com>
2023-07-25 15:25:31 +0900

    배포용 jar 추가 230725

--- src/main/java/com/board/whitelabel/controller/BoardRestController.java
+++ src/main/java/com/board/whitelabel/controller/BoardRestController.java
@@ -57,7 +57,7 @@ public class BoardRestController {
 
             model.addAttribute("dto",boardDTO);
 
-            return "window.location.href='whitelabel/modify'";
+            return "<script>window.location.href='whitelabel/modify'</script>";
 
         }
         // 작성자 정보 불일치
